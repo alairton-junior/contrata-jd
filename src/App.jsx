3108ca8ab74c0a19e9128e6850308360e8dd191b
Filipe <diasjoaum@gmail.com>
2024-07-09 23:56:43 -0300

    Adicionado os icones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-
 function App() {
-
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -8,4 +6,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
